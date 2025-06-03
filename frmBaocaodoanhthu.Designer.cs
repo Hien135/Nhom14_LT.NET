60ce4b53f2a0930293c1f73245ecd43cdc9bb89d
lthpdh <huyenthule3@gmail.com>
2025-06-03 17:58:38 +0700

    Add files via upload

---
+++ frmBaocaodoanhthu.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Nhom14_LT.NET.Forms
+{
+    partial class frmBaocaodoanhthu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblDenngay = new System.Windows.Forms.Label();
+            this.lblTungay = new System.Windows.Forms.Label();
+            this.lblLoaithongke = new System.Windows.Forms.Label();
+            this.dtTungay = new System.Windows.Forms.DateTimePicker();
+            this.dtDenngay = new System.Windows.Forms.DateTimePicker();
+            this.cboLoaithongke = new System.Windows.Forms.ComboBox();
+            this.btnThongke = new System.Windows.Forms.Button();
+            this.btnXuatexcel = new System.Windows.Forms.Button();
+            this.btnDong = new System.Windows.Forms.Button();
+            this.chartDoanhthu = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dgvDoanhthu = new System.Windows.Forms.DataGridView();
+//            this.lblTongDoanhThu = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chartDoanhthu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDoanhthu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkSalmon;
+            this.label1.Location = new System.Drawing.Point(102, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(365, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BÁO CÁO DOANH THU";
+            // 
+            // lblDenngay
+            // 
+            this.lblDenngay.AutoSize = true;
+            this.lblDenngay.Location = new System.Drawing.Point(31, 117);
+            this.lblDenngay.Name = "lblDenngay";
+            this.lblDenngay.Size = new System.Drawing.Size(56, 13);
+            this.lblDenngay.TabIndex = 1;
+            this.lblDenngay.Text = "Đến ngày:";
+            // 
+            // lblTungay
+            // 
+            this.lblTungay.AutoSize = true;
+            this.lblTungay.Location = new System.Drawing.Point(38, 90);
+            this.lblTungay.Name = "lblTungay";
+            this.lblTungay.Size = new System.Drawing.Size(49, 13);
+            this.lblTungay.TabIndex = 2;
+            this.lblTungay.Text = "Từ ngày:";
+            // 
+            // lblLoaithongke
+            // 
+            this.lblLoaithongke.AutoSize = true;
+            this.lblLoaithongke.Location = new System.Drawing.Point(12, 59);
+            this.lblLoaithongke.Name = "lblLoaithongke";
+            this.lblLoaithongke.Size = new System.Drawing.Size(75, 13);
+            this.lblLoaithongke.TabIndex = 3;
+            this.lblLoaithongke.Text = "Loại thống kê:";
+            // 
+            // dtTungay
+            // 
+            this.dtTungay.Location = new System.Drawing.Point(109, 86);
+            this.dtTungay.Name = "dtTungay";
+            this.dtTungay.Size = new System.Drawing.Size(200, 20);
+            this.dtTungay.TabIndex = 4;
+            // 
+            // dtDenngay
+            // 
+            this.dtDenngay.Location = new System.Drawing.Point(109, 117);
+            this.dtDenngay.Name = "dtDenngay";
+            this.dtDenngay.Size = new System.Drawing.Size(200, 20);
+            this.dtDenngay.TabIndex = 5;
+            // 
+            // cboLoaithongke
+            // 
+            this.cboLoaithongke.FormattingEnabled = true;
+            this.cboLoaithongke.Location = new System.Drawing.Point(109, 59);
+            this.cboLoaithongke.Name = "cboLoaithongke";
+            this.cboLoaithongke.Size = new System.Drawing.Size(200, 21);
+            this.cboLoaithongke.TabIndex = 6;
+            // 
+            // btnThongke
+            // 
+            this.btnThongke.Location = new System.Drawing.Point(15, 151);
+            this.btnThongke.Name = "btnThongke";
+            this.btnThongke.Size = new System.Drawing.Size(75, 23);
+            this.btnThongke.TabIndex = 7;
+            this.btnThongke.Text = "Thống kê";
+            this.btnThongke.UseVisualStyleBackColor = true;
+            this.btnThongke.Click += new System.EventHandler(this.btnThongke_Click);
+            // 
+            // btnXuatexcel
+            // 
+            this.btnXuatexcel.Location = new System.Drawing.Point(122, 151);
+            this.btnXuatexcel.Name = "btnXuatexcel";
+            this.btnXuatexcel.Size = new System.Drawing.Size(75, 23);
+            this.btnXuatexcel.TabIndex = 8;
+            this.btnXuatexcel.Text = "Xuất excel";
+            this.btnXuatexcel.UseVisualStyleBackColor = true;
+//            this.btnXuatexcel.Click += new System.EventHandler(this.btnXuatexcel_Click);
+            // 
+            // btnDong
+            // 
+            this.btnDong.Location = new System.Drawing.Point(234, 151);
+            this.btnDong.Name = "btnDong";
+            this.btnDong.Size = new System.Drawing.Size(75, 23);
+            this.btnDong.TabIndex = 9;
+            this.btnDong.Text = "Đóng";
+            this.btnDong.UseVisualStyleBackColor = true;
+            // 
+            // chartDoanhthu
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartDoanhthu.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartDoanhthu.Legends.Add(legend1);
+            this.chartDoanhthu.Location = new System.Drawing.Point(355, 59);
+            this.chartDoanhthu.Name = "chartDoanhthu";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartDoanhthu.Series.Add(series1);
+            this.chartDoanhthu.Size = new System.Drawing.Size(258, 115);
+            this.chartDoanhthu.TabIndex = 10;
+            this.chartDoanhthu.Text = "chart1";
+            // 
+            // dgvDoanhthu
+            // 
+            this.dgvDoanhthu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDoanhthu.Location = new System.Drawing.Point(15, 194);
+            this.dgvDoanhthu.Name = "dgvDoanhthu";
+            this.dgvDoanhthu.Size = new System.Drawing.Size(294, 114);
+            this.dgvDoanhthu.TabIndex = 11;
+            // 
+            // lblTongDoanhThu
+            // 
+          
+            // frmBaocaodoanhthu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(671, 376);
+          //  this.Controls.Add(this.lblTongDoanhThu);
+            this.Controls.Add(this.dgvDoanhthu);
+            this.Controls.Add(this.chartDoanhthu);
+            this.Controls.Add(this.btnDong);
+            this.Controls.Add(this.btnXuatexcel);
+            this.Controls.Add(this.btnThongke);
+            this.Controls.Add(this.cboLoaithongke);
+            this.Controls.Add(this.dtDenngay);
+            this.Controls.Add(this.dtTungay);
+            this.Controls.Add(this.lblLoaithongke);
+            this.Controls.Add(this.lblTungay);
+            this.Controls.Add(this.lblDenngay);
+            this.Controls.Add(this.label1);
+            this.Name = "frmBaocaodoanhthu";
+            this.Text = "frmBaocaodoanhthu";
+            this.Load += new System.EventHandler(this.frmBaocaodoanhthu_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chartDoanhthu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDoanhthu)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDenngay;
+        private System.Windows.Forms.Label lblTungay;
+        private System.Windows.Forms.Label lblLoaithongke;
+        private System.Windows.Forms.DateTimePicker dtTungay;
+        private System.Windows.Forms.DateTimePicker dtDenngay;
+        private System.Windows.Forms.ComboBox cboLoaithongke;
+        private System.Windows.Forms.Button btnThongke;
+        private System.Windows.Forms.Button btnXuatexcel;
+        private System.Windows.Forms.Button btnDong;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartDoanhthu;
+        private System.Windows.Forms.DataGridView dgvDoanhthu;
+      //  private System.Windows.Forms.Label lblTongDoanhThu;
+    }
+}
---
+++ frmBaocaodoanhthu.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Windows.Forms.DataVisualization.Charting;
+
+
+        namespace Nhom14_LT.NET.Forms
+    {
+        public partial class frmBaocaodoanhthu : Form
+        {
+            SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=QuanLyThueSach;Integrated Security=True");
+
+            public frmBaocaodoanhthu()
+            {
+                InitializeComponent();
+                this.Load += frmBaocaodoanhthu_Load;
+                btnThongke.Click += btnThongke_Click;
+                btnXuatexcel.Click += btnXuatexcel_Click;
+                btnDong.Click += btnDong_Click;
+            }
+      
+        private void btnDong_Click(object sender, EventArgs e)
+            {
+                this.Close();
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+            /*private void frmBaocaodoanhthu_Load(object sender, EventArgs e)
+        {
+            cboLoaithongke.Items.AddRange(new string[] { "Ngày", "Khách hàng", "Sách" });
+            cboLoaithongke.SelectedIndex = 0;
+        }*/
+        private void frmBaocaodoanhthu_Load(object sender, EventArgs e)
+        {
+            if (cboLoaithongke.Items.Count == 0)
+            {
+                cboLoaithongke.Items.AddRange(new string[] { "Ngày", "Khách hàng", "Sách", "Nhân viên" });
+            }
+            cboLoaithongke.SelectedIndex = 0;
+        }
+
+        private void btnThongke_Click(object sender, EventArgs e)
+        {
+            if (cboLoaithongke.Text == "Ngày") ThongKeTheoNgay();
+            else if (cboLoaithongke.Text == "Khách hàng") ThongKeTheoKhachHang();
+            else if (cboLoaithongke.Text == "Sách") ThongKeTheoSach();
+            else if (cboLoaithongke.Text == "Nhân viên") ThongKeTheoNhanVien();
+        }
+        private void HienThiTongDoanhThu(DataTable dt)
+        {
+            decimal tong = 0;
+            foreach (DataRow row in dt.Rows)
+            {
+                tong += Convert.ToDecimal(row["DoanhThu"]);
+            }
+            MessageBox.Show($"Tổng doanh thu: {tong:N0} VND", "Tổng kết", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void ThongKeTheoNhanVien()
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Open) conn.Close();
+                conn.Open();
+                string sql = @"SELECT nv.TenNV, COUNT(ts.MaThue) AS SoLanTra, SUM(ts.TongTien) AS DoanhThu
+                               FROM TraSach ts
+                               JOIN ThueSach th ON ts.MaThue = th.MaThue
+                               JOIN NhanVien nv ON th.MaNV = nv.MaNV
+                               WHERE ts.NgayTra BETWEEN @TuNgay AND @DenNgay
+                               GROUP BY nv.TenNV ORDER BY DoanhThu DESC";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@TuNgay", dtTungay.Value.Date);
+                cmd.Parameters.AddWithValue("@DenNgay", dtDenngay.Value.Date);
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dgvDoanhthu.DataSource = dt;
+
+                chartDoanhthu.Series.Clear();
+                Series series = new Series("Doanh thu theo nhân viên");
+                series.ChartType = SeriesChartType.Column;
+                foreach (DataRow row in dt.Rows)
+                {
+                    series.Points.AddXY(row["TenNV"].ToString(), Convert.ToDouble(row["DoanhThu"]));
+                }
+                chartDoanhthu.Series.Add(series);
+
+                HienThiTongDoanhThu(dt);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi thống kê: " + ex.Message);
+            }
+            finally { conn.Close(); }
+        }
+
+        private void ThongKeTheoNgay()
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Open) conn.Close();
+                conn.Open();
+                string sql = @"SELECT NgayTra, SUM(TongTien) AS DoanhThu
+                               FROM TraSach
+                               WHERE NgayTra BETWEEN @TuNgay AND @DenNgay
+                               GROUP BY NgayTra ORDER BY NgayTra";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@TuNgay", dtTungay.Value.Date);
+                cmd.Parameters.AddWithValue("@DenNgay", dtDenngay.Value.Date);
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dgvDoanhthu.DataSource = dt;
+
+                chartDoanhthu.Series.Clear();
+                Series series = new Series("Doanh thu theo ngày");
+                series.ChartType = SeriesChartType.Column;
+                foreach (DataRow row in dt.Rows)
+                {
+                    series.Points.AddXY(Convert.ToDateTime(row["NgayTra"]).ToShortDateString(), Convert.ToDouble(row["DoanhThu"]));
+                }
+                chartDoanhthu.Series.Add(series);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi thống kê: " + ex.Message);
+            }
+            finally { conn.Close(); }
+        }
+
+        private void ThongKeTheoKhachHang()
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Open) conn.Close();
+                conn.Open();
+                string sql = @"SELECT kh.TenKhach, COUNT(ts.MaThue) AS SoLanTra, SUM(ts.TongTien) AS DoanhThu
+                               FROM TraSach ts
+                               JOIN ThueSach th ON ts.MaThue = th.MaThue
+                               JOIN KhachHang kh ON th.MaKhach = kh.MaKhach
+                               WHERE ts.NgayTra BETWEEN @TuNgay AND @DenNgay
+                               GROUP BY kh.TenKhach ORDER BY DoanhThu DESC";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@TuNgay", dtTungay.Value.Date);
+                cmd.Parameters.AddWithValue("@DenNgay", dtDenngay.Value.Date);
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dgvDoanhthu.DataSource = dt;
+
+                chartDoanhthu.Series.Clear();
+                Series series = new Series("Doanh thu theo khách hàng");
+                series.ChartType = SeriesChartType.Column;
+                foreach (DataRow row in dt.Rows)
+                {
+                    series.Points.AddXY(row["TenKhach"].ToString(), Convert.ToDouble(row["DoanhThu"]));
+                }
+                chartDoanhthu.Series.Add(series);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi thống kê: " + ex.Message);
+            }
+            finally { conn.Close(); }
+        }
+
+        private void ThongKeTheoSach()
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Open) conn.Close();
+                conn.Open();
+
+                string sql = @"SELECT s.TenSach, ctts.ThanhTien
+FROM ChiTietTraSach ctts
+JOIN Sach s ON ctts.MaSach = s.MaSach";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@TuNgay", dtTungay.Value.Date.ToString("yyyy-MM-dd"));
+                cmd.Parameters.AddWithValue("@DenNgay", dtDenngay.Value.Date.ToString("yyyy-MM-dd"));
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                dgvDoanhthu.DataSource = dt;
+
+                chartDoanhthu.Series.Clear();
+                Series series = new Series("Doanh thu theo sách");
+                series.ChartType = SeriesChartType.Column;
+                foreach (DataRow row in dt.Rows)
+                {
+                    string tenSach = row["TenSach"].ToString();
+                    double doanhThu = Convert.ToDouble(row["DoanhThu"]);
+                    series.Points.AddXY(tenSach, doanhThu);
+                }
+                chartDoanhthu.Series.Add(series);
+
+                HienThiTongDoanhThu(dt);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi thống kê sách: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+
+
+
+
+
+
+        private void btnXuatexcel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Excel.Application excelApp = new Excel.Application();
+                excelApp.Application.Workbooks.Add(Type.Missing);
+                Excel._Worksheet worksheet = (Excel._Worksheet)excelApp.ActiveSheet;
+
+                for (int i = 1; i < dgvDoanhthu.Columns.Count + 1; i++)
+                {
+                    worksheet.Cells[1, i] = dgvDoanhthu.Columns[i - 1].HeaderText;
+                }
+
+                for (int i = 0; i < dgvDoanhthu.Rows.Count; i++)
+                {
+                    for (int j = 0; j < dgvDoanhthu.Columns.Count; j++)
+                    {
+                        worksheet.Cells[i + 2, j + 1] = dgvDoanhthu.Rows[i].Cells[j].Value?.ToString();
+                    }
+                }
+
+                excelApp.Visible = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi xuất Excel: " + ex.Message);
+            }
+        }
+
+       
+    }
+}
